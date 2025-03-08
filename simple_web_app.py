5ee78d793fa284e436eb443c7b11ee01b93710ec
Alistair Mckeown <alistair.mckeown@poseidonlogic.com>
2025-03-08 14:14:36 +0000

    Changed

--- simple_web_app.py
+++ simple_web_app.py
@@ -18,7 +18,7 @@ def hello_world():
             </style>
         </head>
         <body>
-            Hello, World! Roman has long hair and a tail
+            Hello, World! Leo has long hair and a tail
         </body>
     </html>
     """
