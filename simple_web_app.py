5e0a49779986b723467b1cc0ef9758d5ad955ae2
Alistair Mckeown <alistair.mckeown@poseidonlogic.com>
2025-03-08 11:13:24 +0000

    CHanged message

--- simple_web_app.py
+++ simple_web_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! Roman has long hair"
+    return "Hello, World! Roman has long hair and a tail"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
