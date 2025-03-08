c877cf68736f95c87418697527075ea6c628d820
Alistair Mckeown <alistair.mckeown@poseidonlogic.com>
2025-03-08 11:27:55 +0000

    Changed font

--- simple_web_app.py
+++ simple_web_app.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! Roman has long hair and a tail"
+    return """
+    <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    font-size: 36px;
+                    font-weight: bold;
+                    text-align: center;
+                    margin-top: 20%;
+                }
+            </style>
+        </head>
+        <body>
+            Hello, World! Roman has long hair and a tail
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
